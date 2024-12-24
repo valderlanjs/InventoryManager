19c37f8fa1984cd103a878cdc7925de6f0c6d306
Valderlan Silva <valderlanjosr15@gmail.com>
2024-12-24 18:33:11 -0300

    feat: Finalizado o frontend do navbar e sidebar, porém sem funcionalidades implementadas ainda, apenas a parte visual, como: Sidebar responsivo para diferentes telas; Navbar com os icones de configurações, usuário, nome de usuário, alternador de tema da aplicação e input de pesquisa.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
+import Dashboard from "@/app/dashboard/page";
+
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello
-      </main>
+      <Dashboard />
   );
 }
