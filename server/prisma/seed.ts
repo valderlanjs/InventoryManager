c0114ff57474bcd3fa3b04a7e0bfe35f4ccf6ce2
Valderlan Silva <valderlanjosr15@gmail.com>
2025-01-13 11:37:29 -0300

    Realizado a conexão com o banco de dados e incluidos alguns dados para os produtos

---
+++ server/prisma/seed.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
+const prisma = new PrismaClient();
+
+async function deleteAllData(orderedFileNames: string[]) {
+  const modelNames = orderedFileNames.map((fileName) => {
+    const modelName = path.basename(fileName, path.extname(fileName));
+    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
+  });
+
+  for (const modelName of modelNames) {
+    const model: any = prisma[modelName as keyof typeof prisma];
+    if (model) {
+      await model.deleteMany({});
+      console.log(`Dados apagados de ${modelName}`);
+    } else {
+      console.error(
+        `Model ${modelName} não encontrado. Certifique-se de que o nome do modelo esteja especificado corretamente.`
+      );
+    }
+  }
+}
+
+async function main() {
+  const dataDirectory = path.join(__dirname, "seedData");
+
+  const orderedFileNames = [
+    "products.json",
+    "expenseSummary.json",
+    "sales.json",
+    "salesSummary.json",
+    "purchases.json",
+    "purchaseSummary.json",
+    "users.json",
+    "expenses.json",
+    "expenseByCategory.json",
+  ];
+
+  await deleteAllData(orderedFileNames);
+
+  for (const fileName of orderedFileNames) {
+    const filePath = path.join(dataDirectory, fileName);
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const modelName = path.basename(fileName, path.extname(fileName));
+    const model: any = prisma[modelName as keyof typeof prisma];
+
+    if (!model) {
+      console.error(`Nenhum modelo Prisma corresponde ao nome do arquivo: ${fileName}`);
+      continue;
+    }
+
+    for (const data of jsonData) {
+      await model.create({
+        data,
+      });
+    }
+
+    console.log(`Semeado ${modelName} com dados de ${fileName}`);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
