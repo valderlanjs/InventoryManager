b40a4f7a8e14854a988a99074f3d26bde38f6944
Valderlan Silva <valderlanjosr15@gmail.com>
2025-01-17 09:26:40 -0300

    ajustado problema na logica da geração do productId ao criar produtos

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
-import { v4 } from "uuid";
 import Header from "@/app/(components)/Header";
 
 type ProductFormData = {
@@ -21,7 +20,6 @@ const CreateProductModal = ({
   onCreate,
 }: CreateProductModalProps) => {
   const [formData, setFormData] = useState({
-    productId: v4(),
     name: "",
     price: 0,
     stockQuantity: 0,
