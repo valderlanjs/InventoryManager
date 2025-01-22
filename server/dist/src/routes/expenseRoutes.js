6325549cbf1031261b3759630ae480f1acfa772f
Valderlan Silva <valderlanjosr15@gmail.com>
2025-01-22 10:10:48 -0300

    Removido 'dist' do repositório e adicionado ao gitignore

--- server/dist/src/routes/expenseRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const expenseControler_1 = require("../controllers/expenseControler");
-const router = (0, express_1.Router)();
-router.get("/", expenseControler_1.getExpensesByCategory);
-exports.default = router;
